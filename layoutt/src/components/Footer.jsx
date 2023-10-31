4f06a380280d7dc13d1957498759b8ce8a16c6f0
DaniiGomezz <gomezjesushd@gmail.com>
2023-10-30 23:01:56 -0300

    cambios en el footer

--- layoutt/src/components/Footer.jsx
+++ layoutt/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Footer = () => {
   return (
     <div className="container-fluide-xpand-lg ">
-    <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top bg-warning">
+    <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my- border-top bg-warning">
       <p class="col-md-4 mb-0 text-body-secondary">© 2023 Company, Inc</p>
   
       <a href="/" className="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
